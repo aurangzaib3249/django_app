3f829044bafc05de3b37a7f25ad0542df81c1ca5
Muhammad Aurangzaib <muhammad.aurangzaib@codefulcrum.com>
2022-07-26 19:11:07 +0500

    add heroku files

--- django_app/settings.py
+++ django_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3bl9&)^)38q=mn+#m5&rys6j3d#-emjcr&bum=^0%eiww^0g+k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
