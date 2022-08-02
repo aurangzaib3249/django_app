68fe87afc054af84f87e6396f59ec08dc774592e
Muhammad Aurangzaib <muhammad.aurangzaib@codefulcrum.com>
2022-08-02 13:58:14 +0500

    search filters

--- app/admin.py
+++ app/admin.py
@@ -16,6 +16,7 @@ from django.core.exceptions import ValidationError
 # Register your models here.
 class UserAdmin(BaseUserAdmin):
     add_form = UserForm
+    readonly_fields=("last_login","date_joined",'is_superuser',)
     model = User
     list_display = ('full_name','email','phone', 'is_superuser','is_active')
     list_filter = ('full_name','email','phone','is_superuser', 'is_active','date_joined')
@@ -36,4 +37,95 @@ class UserAdmin(BaseUserAdmin):
             return list()
         return super(UserAdmin, self).get_inline_instances(request, obj)
 
+class ItemFilterByDiscountedPrice(admin.SimpleListFilter):
+    title="Price"
+    parameter_name = 'item_discounted_price'
+
+    def lookups(self, request,model_admin):
+        return (
+            ("Discount Items","Discount Items"),
+            ("Non Discount Items","Non Discount Items")
+        )
+    def queryset(self, request,queryset):
+        if not self.value():
+            return queryset
+        if self.value()=="Discount Items":
+            return queryset.filter(item_discounted_price__isnull=True)
+        if self.value()=="Non Discount Items":
+            return queryset.filter(item_discounted_price__isnull=False)
+class ItemFilterByUpdated(admin.SimpleListFilter):
+    title="Update Status"
+    parameter_name = 'updated_at'
+    def lookups(self, request,model_admin):
+        return (
+            ("Updated Items","Updated Items"),
+            ("Non Updated Items","Non Updated Items")
+        )
+    def queryset(self, request,queryset):
+        if not self.value():
+            return queryset
+        if self.value()=="Updated Items":
+            return queryset.filter(updated_at__isnull=True)
+        if self.value()=="Non Updated Items":
+            return queryset.filter(updated_at__isnull=False)
+class ItemFilterStatus(admin.SimpleListFilter):
+    title="Available Status"
+    parameter_name = 'is_available'
+
+    def lookups(self, request,model_admin):
+        return (
+            ("Available","Available"),
+            ("Not Available","Not Available")
+        )
+    def queryset(self, request,queryset):
+        if not self.value():
+            return queryset
+        if self.value()=="Available":
+            return queryset.filter(item_remaining_qty__gt=0)
+        if self.value()=="Not Available":
+            return queryset.filter(item_remaining_qty__lte=0)
+class ItemFilterCategory(admin.SimpleListFilter):
+    title="Categories"
+    parameter_name = 'category'
+    def lookups(self, request,model_admin):
+        types = set([t.category for t in ItemCategory.objects.all().distinct()])
+        tup=zip(types, types) 
+        print(tup)
+        return  tup   
+    def queryset(self, request,queryset):
+        if not self.value():
+            return queryset
+        if self.value():
+            return queryset.filter(category__category=self.value())
+        
+    
+        
+class ItemAdmin(admin.ModelAdmin):
+    model=Item
+    
+    readonly_fields=("item_code","slug")
+    list_display=("id","item_code","item_name","category","get_item_price","item_remaining_qty","is_available","is_active")
+    list_filter=("is_active","created_at",ItemFilterCategory,ItemFilterByDiscountedPrice,ItemFilterByUpdated)
+    fieldsets=(
+        ("Item Details",{"fields":("item_name","category","item_price","item_discounted_price","item_qty","item_remaining_qty","is_active",)}),
+        ("Read only Fields",{"fields":("item_code","slug")}),
+    )
+    add_fieldsets=(
+        (None,{
+            "classes":("wide",),
+            "fields":("item_code","item_name","category","item_price","item_discounted_price","item_qty","item_remaining_qty","is_active")
+        })
+    )
+    search_fields=("item_name",)
+    ordering=("-id",)
+    autocomplete_fields=("category",)
+    def get_inline_instances(self, request,obj=None):
+        if not obj:
+            return list()
+        return super().get_inline_instances(request, obj)
+    
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields=("category",)
+admin.site.register(Item, ItemAdmin)
+admin.site.register(ItemCategory,CategoryAdmin)
 admin.site.register(User, UserAdmin)
--- app/models.py
+++ app/models.py
@@ -3,8 +3,11 @@ import django
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser,AbstractUser,BaseUserManager
 # Create your models here.
-from django.utils.translation import gettext_lazy as _
 
+from django.utils.translation import gettext_lazy as _
+from django.core.exceptions import ValidationError
+
+from django.utils.text import slugify
 class UserManager(BaseUserManager):
    
     def create_user(self, email, password, **extra_fields):
@@ -42,3 +45,62 @@ class User(AbstractUser):
     address= models.CharField(max_length=200,null=True,blank=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    
+ 
+   
+class ItemCategory(models.Model):
+   
+    category=models.CharField(max_length=20)
+    def __str__(self):
+        return self.category
+   
+    
+    
+    
+    
+    
+class Item(models.Model):
+    item_name=models.CharField(_("Item Name"),max_length=35)
+    item_code=models.CharField(max_length=35,blank=True,null=True)
+    category=models.ForeignKey(ItemCategory,on_delete=models.CASCADE)
+    item_price=models.IntegerField(default=0)
+    item_discounted_price=models.IntegerField(blank=True,null=True)
+    item_qty=models.IntegerField(default=0)
+    item_remaining_qty=models.IntegerField(null=True,blank=True)
+    slug=models.SlugField(null=True,blank=True)
+    is_active=models.BooleanField(default=True)
+    created_at=models.DateTimeField(auto_now_add=True,null=True,blank=True)
+    updated_at=models.DateTimeField(auto_now=True,null=True,blank=True)
+    
+
+    def clean(self):
+        if not self.item_remaining_qty:
+            raise ValidationError({"item_remaining_qty":"Please enter Item remaining Quantity"})
+        if not self.item_name:
+            raise ValidationError({"item_name":"Please enter Item Name"})
+    @property
+    def get_item_price(self):
+        if self.item_discounted_price:
+            return self.item_discounted_price
+        else:
+            return self.item_price
+    @property
+    def is_available(self):
+        if self.item_remaining_qty >0:
+            return "Yes"
+        else:
+            return "No"
+    @property
+    def get_item_remianing_qty(self):
+        return self.item_remaining_qty
+    def save(self,*args, **kwargs):
+        self.full_clean()
+        super(Item,self).save(*args, **kwargs)
+        if not self.item_code:
+            self.item_code=self.category.category+"_"+str(self.id)
+            if not self.slug:
+                self.slug=slugify(self.item_code)
+            self.save()
+    def __str__(self) -> str:
+        return self.item_code
